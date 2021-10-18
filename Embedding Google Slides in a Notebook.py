2500c7ff45285cf22fb7aa68e88d884bc8c98105
koundinyabs <koundinyabs>
2021-10-18 13:57:24 +0000

    Initial commit

---
+++ Embedding Google Slides in a Notebook.py
@@ -0,0 +1,27 @@
+# Databricks notebook source
+# MAGIC %md ##How to embed Google Slides in a cell
+# MAGIC 
+# MAGIC - Figure out the `deck_id` looking at the URL. This should be the string before `/embed` in the URL.
+# MAGIC - Run the cell below which defines the function to embed GSlide using `iframe`
+# MAGIC - Then invoke the function with the `deck_id` and the `slide_number` in any cell you want to embed the deck. The iframe is initialized to start from the `slide_number` you specify. Example:
+# MAGIC 
+# MAGIC `embedGoogleSlides('1d3ck1dfromth3url', '1')`
+
+# COMMAND ----------
+
+def embedGoogleSlides(deck_id, slide_number):
+  
+  displayHTML(f'''
+
+  <iframe
+    src="https://docs.google.com/presentation/d/{deck_id}/embed?slide={slide_number}&rm=minimal"
+    frameborder="0"
+    width="100%"
+    height="700"
+  ></iframe>
+
+  ''')
+
+# COMMAND ----------
+
+embedGoogleSlides('1d3ck1dfromth3url', '1')
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# embed-gslides
+##How to embed Google Slides in a cell
+
+- Figure out the `deck_id` looking at the URL. This should be the string before `/embed` in the URL.
+- Run the cell below which defines the function to embed GSlide using `iframe`
+- Then invoke the function with the `deck_id` and the `slide_number` in any cell you want to embed the deck. The iframe is initialized to start from the `slide_number` you specify. Example:
+
+`embedGoogleSlides('1d3ck1dfromth3url', '1')`
